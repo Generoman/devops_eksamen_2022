6bd10e52532daf3c9488a27788bbb652504a7ed3
Generoman <53780885+Generoman@users.noreply.github.com>
2022-12-08 19:43:33 +0100

    alarms attempt_3

--- infra/alarm.tf
+++ infra/alarm.tf
@@ -12,5 +12,5 @@ resource "aws_cloudwatch_metric_alarm" "test" {
 
   alarm_description = "Goes off when the amount of carts exceeds 5 over 3 periods of 5 minutes"
   insufficient_data_actions = []
-  alarm_actions = [aws_sns_topic.alarms]
+  alarm_actions = [aws_sns_topic.alarms.arn]
 }
