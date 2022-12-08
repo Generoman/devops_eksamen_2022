ce334830140f8b46b35ae74bff6f6c4a8c46e08b
Generoman <53780885+Generoman@users.noreply.github.com>
2022-12-08 19:27:53 +0100

    alarms attempt_1

---
+++ infra/alarm.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_metric_alarm" "test" {
+  alarm_name = "carts-exceeded-5"
+  namespace = var.candidate_id
+  metric_name = "carts.value"
+
+  comparison_operator = "GreaterThanThreshold"
+  threshold = "5"
+  evaluation_periods  = "3"
+  period = "5"
+
+  statistic = "Maximum"
+
+  alarm_description = "Goes off when the amount of carts exceeds 5 over 3 periods of 5 minutes"
+  insufficient_data_actions = []
+  alarm_actions = [aws_sns_topic.alarms]
+}
